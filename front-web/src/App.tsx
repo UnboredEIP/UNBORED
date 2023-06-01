fe58fef68275bd2c5e5e8e61691371ea7d379029
drisskow <idrissa.fall@epitech.eu>
2023-06-01 21:31:35 +0200

    [ADD] first home page with login and register react ts

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./styles/App.module.css";
+import Nav from "./components/Nav/Nav";
+import Sidebar from "./components/sidebar/Sidebar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Overview from "./pages/Overview";
+import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/overview" element={<Overview />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
+        </Routes>
+      </Router>
+      <header className={styles["App-header"]}></header>
+    </>
   );
 }
 
