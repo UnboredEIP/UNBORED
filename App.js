08c6e34ce0d724d75de923510ba4213e994d2e81
Remi SALEH <remi.saleh@epitech.eu>
2023-06-07 18:31:40 +0200

    [ADD] forget password and create account

--- App.js
+++ App.js
@@ -20,8 +20,8 @@ class HomeScreen extends React.Component {
     return (
       <NavigationContainer>
       <View style={styles.container}>
-      <Image style={styles.image} source={require('./assets/logo2.gif')} />
-        <Text style={styles.inscrire}>S'inscrire gratuitement !</Text>
+      <Image style={styles.image4} source={require('./assets/logo2.gif')} />
+        <Text style={styles.inscrire}>Se connecter !</Text>
         <View style={styles.inputView}>
           <TextInput
             style={styles.TextInput}
@@ -31,13 +31,13 @@ class HomeScreen extends React.Component {
         <View style={styles.inputView}>
           <TextInput
             style={styles.TextInput}
-            placeholder="Password"
+            placeholder="Mot de passe"
             secureTextEntry={true}
           />
         </View>
         <TouchableOpacity
           style={styles.loginBtn}
-          onPress={() => this.props.navigation.navigate('Profile')}>
+          onPress={() => this.props.navigation.navigate('Choose')}>
           <Text style={styles.loginText}>LOGIN</Text>
         </TouchableOpacity>
         <TouchableOpacity
@@ -78,74 +78,175 @@ class HomeScreen extends React.Component {
       <View style={styles.listItem}>
         <TouchableOpacity onPress={() =>{setclick(item.pr = item.pr * - 1);}} style={{paddingBottom: -100,borderRadius: 25, borderWidth: 1,borderColor: "#F5F5F5",height:160,width:140, left: -60, backgroundColor : item.pr == 1 ? 'white' : '#E1604D'} }>
         <Image source={{uri:item.photo}}  style={{width:100, height:100, left: 20, bottom: -10}} />
-        <Text style={{paddingBottom: -100, color: item.pr == -1 ? 'white' : '#E1604D', alignItems:"center", width:100 , bottom:-12, right: -25}}>{item.name}</Text>
+        <Text style={{paddingBottom: -100, color: item.pr == -1 ? 'white' : '#E1604D', alignItems:"center", width:100 , bottom:-15, right: -20}}>{item.name}</Text>
         </TouchableOpacity>
       </View>
     );
   }
-  
   class ForgetPasswordScreen extends React.Component {
+    render() {
+      return (
+        <NavigationContainer>
+        <View style={styles.container}>
+        <Image style={styles.image4} source={require('./assets/logo2.gif')} />
+        <Text style={styles.inscrire3}>Mot de passe oublié</Text>
+          <Text style={styles.inscrire4}>Inscrivez votre email pour récuperer votre compte</Text>
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Email"
+            />
+          </View>
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Confirmer l'email"
+              secureTextEntry={true}
+            />
+          </View>
+          <TouchableOpacity
+          style={styles.loginBtn}
+          onPress={() => this.props.navigation.navigate('Home')}>
+          <Text style={styles.loginText}>Confirmer</Text>
+        </TouchableOpacity>
+        </View>
+        </NavigationContainer>
+      );
+          }
+  }
+
+  class CreateAccount extends React.Component {
+    render() {
+      return (
+        <NavigationContainer>
+        <View style={styles.container}>
+        <Image style={styles.image4} source={require('./assets/logo2.gif')} />
+          <Text style={styles.inscrire2}>S'inscrire gratuitement !</Text>
+          <View style={styles.inputView2}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Nom"
+            />
+          </View>
+          <View style={styles.inputView2}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Prénom"
+            />
+          </View>
+          <View style={styles.inputView2}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Email"
+            />
+          </View>
+          <View style={styles.inputView2}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Mot de passe"
+              secureTextEntry={true}
+            />
+          </View>
+          <TouchableOpacity
+            style={styles.loginBtn}
+            onPress={() => this.props.navigation.navigate('Choose')}>
+            <Text style={styles.loginText}>S'inscrire</Text>
+          </TouchableOpacity>
+          <Text style={styles.ContinueravecText}>Ou continuer avec</Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              marginLeft: 20,
+              justifyContent: 'space-evenly',
+            }}>
+            <TouchableOpacity style={styles.oauthBtn}>
+              <Image
+                style={styles.image2}
+                source={require('./assets/google.png')}
+              />
+              <Text style={styles.oauthTxt}>Facebook</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.oauthBtn}>
+              <Image
+                style={styles.image2}
+                source={require('./assets/google.png')}
+              />
+              <Text style={styles.oauthTxt}>Google</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            style={styles.FPBtn}
+            onPress={() => this.props.navigation.navigate('Home')}>
+            <Text><Text style={styles.ForgetText2}>J'ai déja un compte</Text><Text style={styles.ForgetText}>   Se connecter</Text></Text>
+          </TouchableOpacity>
+        </View>
+        </NavigationContainer>
+      );
+          }
+  
+  }
+  class ChoosePreferences extends React.Component {
       state = {
         data:[
             {
-                "name": "Miyah Myles",
+                "name": "         Art",
                 "email": "miyah.myles@gmail.com",
                 "pr":1,
-                "photo": "https://cdn1.iconfinder.com/data/icons/seo-web-17/512/5-512.png"
+                "photo": "https://i.postimg.cc/d1RQyTb0/artwhite.png"
             },
             {
-                "name": "June Cha",
+                "name": "     Musique",
                 "email": "june.cha@gmail.com",
                 "pr":1,
-                "photo": "https:\/\/randomuser.me\/api\/portraits\/women\/44.jpg"
+                "photo": "https://i.postimg.cc/15MtBTvr/musical-note.png"
             },
             {
-                "name": "Iida Niskanen",
+                "name": "      Sport",
                 "email": "iida.niskanen@gmail.com",
                 "pr":1,
-                "photo": "https:\/\/randomuser.me\/api\/portraits\/women\/68.jpg"
+                "photo": "https://i.postimg.cc/g2FWvKWH/sports.png"
             },
             {
-                "name": "Renee Sims",
+                "name": "       Food",
                 "email": "renee.sims@gmail.com",
                 "pr":1,
-                "photo": "https:\/\/randomuser.me\/api\/portraits\/women\/65.jpg"
+                "photo": "https://i.postimg.cc/2j1yGTPs/restaurant.png"
             },
             {
-                "name": "Jonathan Nu\u00f1ez",
+                "name": "      Soirée",
                 "email": "jonathan.nu\u00f1ez@gmail.com",
                 "pr":1,
-                "photo": "https:\/\/randomuser.me\/api\/portraits\/men\/43.jpg"
+                "photo": "https://i.postimg.cc/vHRYzy7n/multiple-users-silhouette.png"
             },
             {
-                "name": "Sasha Ho",
+                "name": "  Technologie",
                 "email": "sasha.ho@gmail.com",
                 "pr":1,
-                "photo": "https:\/\/images.pexels.com\/photos\/415829\/pexels-photo-415829.jpeg?h=350&auto=compress&cs=tinysrgb"
+                "photo": "https://i.postimg.cc/6QH6LGz6/smartphone.png"
             },
             {
-                "name": "Abdullah Hadley",
+                "name": "Photographie",
                 "email": "abdullah.hadley@gmail.com",
                 "pr":1,
-                "photo": "https:\/\/images.unsplash.com\/photo-1507003211169-0a1dd7228f2d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=a72ca28288878f8404a795f39642a46f"
+                "photo": "https://i.postimg.cc/nrbyvF8c/photo-camera-interface-symbol-for-button.png"
             },
             {
-                "name": "Thomas Stock",
+                "name": "     Football",
                 "email": "thomas.stock@gmail.com",
                 "pr":1,
-                "photo": "https:\/\/tinyfac.es\/data\/avatars\/B0298C36-9751-48EF-BE15-80FB9CD11143-500w.jpeg"
+                "photo": "https://i.postimg.cc/2Scg0Fpt/football.png"
             },
             {
-                "name": "Veeti Seppanen",
+                "name": "  Automoblie",
                 "email": "veeti.seppanen@gmail.com",
                 "pr":1,
-                "photo": "https:\/\/randomuser.me\/api\/portraits\/men\/97.jpg"
+                "photo": "https://i.postimg.cc/gjHT83qN/sedan.png"
             },
             {
-                "name": "Bonnie Riley",
+                "name": "         LoL",
                 "email": "bonnie.riley@gmail.com",
                 "pr":1,
-                "photo": "https:\/\/randomuser.me\/api\/portraits\/women\/26.jpg"
+                "photo": "https://i.postimg.cc/8z50fJFm/game-control.png"
             }
         ]
       }
@@ -162,9 +263,9 @@ class HomeScreen extends React.Component {
               numColumns={2}
             />
             <TouchableOpacity
-          style={styles.loginBtn}
-          onPress={() => this.props.navigation.navigate('Profile')}>
-          <Text style={styles.loginText}>LOGIN</Text>
+          style={{width: '80%',right:-31,borderRadius: 25,height: 50,alignItems: 'center',justifyContent: 'center',marginTop: 20,marginBottom: 20,backgroundColor: '#E1604D',borderColor: '#b3b3b3',borderWidth: 1,} }
+          onPress={() => this.props.navigation.navigate('Home')}>
+          <Text style={{fontSize:20}}>Suivant</Text>
         </TouchableOpacity>
           </View>
         );
@@ -177,10 +278,12 @@ class HomeScreen extends React.Component {
     {
       Home: HomeScreen,
       ForgetPassword: ForgetPasswordScreen,
+      Choose: ChoosePreferences,
+      CreateAccount: CreateAccount,
     },
     {
       headerMode:false,
-      initialRouteName: 'Home',
+      initialRouteName: 'CreateAccount',
     },
   );
   const AppContainer = createAppContainer(AppNavigator);
@@ -265,6 +368,11 @@ class HomeScreen extends React.Component {
     width: 200,
     height: 190,
   },
+  image4: {
+    marginBottom: 10,
+    width: 200,
+    height: 190,
+  },
   image2: {
     width: 20,
     height: 20,
@@ -279,6 +387,15 @@ class HomeScreen extends React.Component {
     width: '80%',
     height: 45,
     marginBottom: 20,
+    borderColor: '#b3b3b3',
+    borderWidth: 1,
+  },
+  inputView2: {
+    backgroundColor: '#FFF',
+    borderRadius: 30,
+    width: '80%',
+    height: 45,
+    marginBottom: 10,
     borderColor: '#b3b3b3',
     borderWidth: 1,
   },
@@ -300,6 +417,10 @@ class HomeScreen extends React.Component {
     fontSize: 12,
     color: '#E1604D',
   },
+  ForgetText2: {
+    fontSize: 12,
+    color: 'Black',
+  },
   ContinueravecText: {
     fontSize: 20,
   },
@@ -313,8 +434,40 @@ class HomeScreen extends React.Component {
     marginBottom: 40,
 
   },
+  inscrire4: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    marginBottom: 40,
+
+  },
+  inscrire3: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginBottom: 40,
+
+  },
+  inscrire2: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    height: 30,
+    marginBottom: 10,
+
+  },
   loginBtn: {
     width: '80%',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    marginBottom: 10,
+    backgroundColor: '#E1604D',
+    borderColor: '#b3b3b3',
+    borderWidth: 1,
+  },
+  loginBtn2: {
+    width: '80%',
+    right:-31,
     borderRadius: 25,
     height: 50,
     alignItems: 'center',
