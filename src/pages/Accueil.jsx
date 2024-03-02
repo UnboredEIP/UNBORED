b50ff71eb4fdb33e0de34e4b371ae877b2f3984b
idrssf <idrissa.fall@epitech.eu>
2024-03-02 11:32:31 -0800

    [SWITCH] wdws to ubt

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -136,14 +136,14 @@ const Accueil3 = ({ navigation }) => {
                   }}
                 ></View>
                 <View style={{ flexDirection: "row" }}>
-                  <Buttons
+                  {/* <Buttons
                     texte="deco"
                     width="30%"
                     onPress={async () => {
                       await AsyncStorage.removeItem("authToken");
                       navigation.replace("Login2");
                     }}
-                  />
+                  /> */}
                   <TouchableOpacity
                     style={{
                       width: 44,
