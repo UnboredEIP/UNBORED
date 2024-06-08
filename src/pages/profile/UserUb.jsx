df8ec28d840e58c07a78999e230988a2039102b0
idrssf <idrissa.fall@epitech.eu>
2024-06-07 21:52:30 -0700

    [UPDATE] UserUb Page

--- src/pages/profile/UserUb.jsx
+++ src/pages/profile/UserUb.jsx
@@ -149,36 +149,6 @@ function extractTime(dateTimeString) {
   return timeString;
 }
 
-const renderStars = (ratings) => {
-  let totalStars = 0;
-  ratings.forEach((rating) => {
-    totalStars += parseInt(rating.stars); // Sum up all the stars
-  });
-  const averageStars = Math.round(totalStars / ratings.length); // Calculate average stars and round to nearest integer
-  const stars = [];
-  const maxStars = 5; // Maximum number of stars
-  for (let i = 1; i <= maxStars; i++) {
-    if (i <= averageStars) {
-      stars.push(
-        <Image
-          key={i}
-          style={{ width: screenHeight * 0.04, height: screenHeight * 0.04 }}
-          source={startFilled}
-        />
-      );
-    } else {
-      stars.push(
-        <Image
-          key={i}
-          style={{ width: screenHeight * 0.04, height: screenHeight * 0.04 }}
-          source={startUnfilled}
-        />
-      );
-    }
-  }
-  return stars;
-};
-
 const UserUbPage = ({ navigation }) => {
   const [userData, setUserData] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
@@ -186,20 +156,6 @@ const UserUbPage = ({ navigation }) => {
   const [participents, setParticipents] = useState([]);
   const [isEnroll, setIsEnroll] = useState(NOT_ENROLL);
   const ubService = new UbService();
-
-  //   function userIsEnroll(events, id) {
-  //     // for (const invitation of invitations) {
-  //     for (const event of events) {
-  //       if (event === id) {
-  //         setIsEnroll(IS_ENROLL);
-  //         // console.log("ENROLL");
-  //         return true;
-  //       }
-  //       // }
-  //     }
-
-  //     return false; // ID not found in invitations
-  //   }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -224,7 +180,7 @@ const UserUbPage = ({ navigation }) => {
     };
 
     fetchData();
-  }, [userData]);
+  }, [navigation]);
 
   if (
     userData === null ||
@@ -235,7 +191,7 @@ const UserUbPage = ({ navigation }) => {
     return <LoadingPage />;
   } else {
     return (
-      <ScrollView contentContainerStyle={styles.container}>
+      <ScrollView contentContainerStyle={styles().container}>
         <TouchableOpacity
           style={{
             position: "absolute",
@@ -255,11 +211,11 @@ const UserUbPage = ({ navigation }) => {
         </TouchableOpacity>
 
         <Swiper
-          style={styles.swiperContainer}
+          style={styles().swiperContainer}
           loop={false}
           nestedScrollEnabled={true}
         >
-          <View style={styles.slide}>
+          <View style={styles().slide}>
             <Image
               source={{ uri: image.url }}
               style={{
@@ -270,7 +226,7 @@ const UserUbPage = ({ navigation }) => {
               }}
             />
           </View>
-          <View style={styles.slide2}>
+          <View style={styles().slide2}>
             <MyAvatar
               size={200}
               colorSkin={userData.style.head.color}
@@ -282,29 +238,65 @@ const UserUbPage = ({ navigation }) => {
               colorEye={userData.style.eyes.color}
               beard={listBeard[userData.style.beard.id]}
               mouth={listMouth[userData.style.mouth.id]}
-              eyebrow={listEyebrow[userData.style.eyebrows.color]}
+              eyebrow={listEyebrow[userData.style.eyebrows.id]}
             />
           </View>
         </Swiper>
 
-        {/* <Image style={styles.image} source={{ uri: image.url }} /> */}
-        <View style={styles.categoryContainer}>
-          {/* <View style={styles.participantsContainer}>
-            <Text style={styles.participantsText}>
-              {userData.participents.length} personne(s)
-            </Text>
-          </View> */}
-          {/* <TouchableOpacity>
-            <View style={styles.category}>
-              <Text style={styles.categoryText}>{userData.categories[0]}</Text>
+        {/* <Image style={styles().image} source={{ uri: image.url }} /> */}
+        <Text style={styles().title}>{userData.username}</Text>
+        <Text style={styles().description}>{userData.description}</Text>
+        <View
+          style={{
+            position: "relative",
+            alignItems: "center",
+            flexDirection: "column",
+            flex: 1,
+            height: 50 + "%",
+            width: 100 + "%",
+            // paddingBottom: 230,
+          }}
+        >
+          <ScrollView
+            // showsHorizontalScrollIndicator={false}
+            nestedScrollEnabled={true}
+            horizontal={false}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                flexWrap: "wrap",
+                justifyContent: "space-between",
+                alignItems: "center",
+                // width: "100%",
+                paddingHorizontal: screenHeight * 0.005,
+              }}
+            >
+              {userData.preferences.length > 0
+                ? userData.preferences.map((preference, index) => (
+                    <View
+                      style={styles(preference.length).categoryContainer}
+                      key={index}
+                    >
+                      <TouchableOpacity key={index}>
+                        <View
+                          style={styles(preference.length).category}
+                          key={index}
+                        >
+                          <Text
+                            style={styles(preference.length).categoryText}
+                            key={index}
+                          >
+                            {preference}
+                          </Text>
+                        </View>
+                      </TouchableOpacity>
+                    </View>
+                  ))
+                : {}}
             </View>
-          </TouchableOpacity> */}
+          </ScrollView>
         </View>
-        <Text style={styles.title}>{userData.username}</Text>
-        {/* <Text style={styles.date}>{formatDate(userData.start_date)}</Text> */}
-        {/* <Text style={styles.time}>
-          Heure début: {extractTime(userData.start_date)}
-        </Text> */}
         <View
           style={{
             marginVertical: 10,
@@ -316,7 +308,7 @@ const UserUbPage = ({ navigation }) => {
             setModalVisible(true);
           }}
         /> */}
-        {/* <View style={styles.ratingContainer}>{renderStars(userData.rate)}</View> */}
+        {/* <View style={styles().ratingContainer}>{renderStars(userData.rate)}</View> */}
         <Modal
           animationType="fade"
           transparent={true}
@@ -364,119 +356,120 @@ const UserUbPage = ({ navigation }) => {
   }
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: screenHeight * 0.00027,
-    alignItems: "center",
-    justifyContent: "center",
-    // paddingTop: screenHeight / 10,
-  },
-  backButton: {
-    position: "absolute",
-    top: 20,
-    left: 20,
-    zIndex: 1,
-  },
-  backIcon: {
-    width: 30,
-    height: 30,
-  },
-  image: {
-    height: screenHeight * 0.4,
-    width: screenHeight * 0.49,
-  },
-  title: {
-    textAlign: "center",
-    color: "#E1604D",
-    fontWeight: "600",
-    fontSize: screenHeight * 0.03,
-  },
-
-  time: {
-    textAlign: "left",
-    color: "black",
-    fontWeight: "500",
-    fontSize: screenHeight * 0.04,
-    marginTop: screenHeight * 0.017,
-  },
-  categoryContainer: {
-    alignSelf: "flex-start",
-    flexDirection: "row",
-    marginLeft: screenWidth * 0.05,
-    marginTop: screenHeight * 0.01,
-    alignItems: "center",
-  },
-  category: {
-    width: screenHeight * 0.1,
-    height: screenHeight * 0.04,
-    borderWidth: 1,
-    borderColor: "#E1604D",
-    borderRadius: 100,
-    justifyContent: "center",
-  },
-  categoryText: {
-    fontWeight: "600",
-    fontSize: screenHeight * 0.02,
-    color: "#E1604D",
-    textAlign: "center",
-  },
-  participantsContainer: {
-    marginTop: screenHeight * 0.017,
-  },
-  participantsText: {
-    fontWeight: "500",
-    fontSize: screenHeight * 0.027,
-  },
-  locationContainer: {
-    marginTop: screenHeight * 0.069,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  locationTextContainer: {
-    flexDirection: "row",
-  },
-  locationIcon: {
-    width: screenHeight * 0.04,
-    height: screenHeight * 0.04,
-    marginTop: screenHeight * 0.001,
-  },
-  locationText: {
-    marginLeft: screenHeight * 0.02,
-    fontSize: screenHeight * 0.02,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  vectorIcon: {
-    width: screenHeight * 0.01,
-    height: screenHeight * 0.03,
-    marginTop: screenHeight * 0.0006,
-  },
-  ratingContainer: {
-    flexDirection: "row",
-    alignSelf: "center",
-    marginTop: screenHeight * 0.01,
-  },
-  swiperContainer: {
-    // width: "100%",
-    height: 150,
-    marginBottom: 10,
-  },
-  slide: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10,
-    overflow: "hidden",
-  },
-  slide2: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10,
-    overflow: "hidden",
-    right: screenWidth * 0.25,
-    paddingBottom: 140,
-  },
-});
+const styles = (len) => {
+  return StyleSheet.create({
+    container: {
+      flexGrow: screenHeight * 0.0001,
+      alignItems: "center",
+      justifyContent: "center",
+      paddingTop: screenHeight * 0.03,
+    },
+    backButton: {
+      position: "absolute",
+      top: 20,
+      left: 20,
+      zIndex: 1,
+    },
+    backIcon: {
+      width: 30,
+      height: 30,
+    },
+    image: {
+      height: screenHeight * 0.4,
+      width: screenHeight * 0.49,
+    },
+    title: {
+      textAlign: "center",
+      color: "#E1604D",
+      fontWeight: "600",
+      fontSize: screenHeight * 0.04,
+      paddingTop: screenHeight * 0.01,
+    },
+    description: {
+      textAlign: "center",
+      // color: "#E1604D",
+      // fontWeight: "6",
+      fontSize: screenHeight * 0.02,
+      // paddingTop: screenHeight * 0.01,
+    },
+    categoryContainer: {
+      alignSelf: "flex-start",
+      flexDirection: "row",
+      paddingLeft: screenWidth * 0.015,
+      paddingTop: screenHeight * 0.01,
+      alignItems: "center",
+    },
+    category: {
+      width: screenHeight * 0.1,
+      height: screenHeight * 0.04,
+      borderWidth: 1,
+      borderColor: "#E1604D",
+      borderRadius: 100,
+      justifyContent: "center",
+    },
+    categoryText: {
+      fontWeight: "600",
+      fontSize: screenHeight * 0.02 - len * 0.5,
+      color: "#E1604D",
+      textAlign: "center",
+    },
+    participantsContainer: {
+      paddingTop: screenHeight * 0.017,
+    },
+    participantsText: {
+      fontWeight: "500",
+      fontSize: screenHeight * 0.027,
+    },
+    locationContainer: {
+      paddingTop: screenHeight * 0.069,
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    locationTextContainer: {
+      flexDirection: "row",
+    },
+    locationIcon: {
+      width: screenHeight * 0.04,
+      height: screenHeight * 0.04,
+      paddingTop: screenHeight * 0.001,
+    },
+    locationText: {
+      paddingLeft: screenHeight * 0.02,
+      fontSize: screenHeight * 0.02,
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+    vectorIcon: {
+      width: screenHeight * 0.01,
+      height: screenHeight * 0.03,
+      paddingTop: screenHeight * 0.0006,
+    },
+    ratingContainer: {
+      flexDirection: "row",
+      alignSelf: "center",
+      paddingTop: screenHeight * 0.01,
+    },
+    swiperContainer: {
+      height: screenHeight * 0.24,
+      marginTop: screenHeight * 0.05,
+    },
+    slide: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: 10,
+      overflow: "hidden",
+    },
+    slide2: {
+      flex: 1,
+      // justifyContent: "center",
+      alignItems: "center",
+      borderRadius: 10,
+      overflow: "hidden",
+      right: screenWidth * 0.25,
+      // paddingBottom: 140,
+    },
+  });
+};
 
 export default UserUbPage;
